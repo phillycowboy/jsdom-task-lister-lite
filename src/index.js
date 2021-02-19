2453b931a086f1e9f4a5f335c0b9dd13ea1e27a4
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-19 17:22:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskDescription = document.getElementById("create-task-form")
+  
+  const createNewTask = document.getElementById("create-new-task")
+  
+  const tasks = document.getElementById("tasks")
+  
+  newTaskDescription.addEventListener("submit", handleSubmitEvent)
+  
+  
 });
+
+function handleSubmitEvent(e) {
+  e.preventDefault()
+  let taskValue = e.target[0].value 
+  tasks.innerHTML += `
+    <li class="task">${taskValue}</li>
+  `
+
+};
+
